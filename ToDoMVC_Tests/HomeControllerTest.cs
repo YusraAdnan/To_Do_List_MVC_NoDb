168c15cf0a8383878ddcbe954050b970b66a180b
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-10-08 12:47:18 +0200

    test comments

--- ToDoMVC_Tests/HomeControllerTest.cs
+++ ToDoMVC_Tests/HomeControllerTest.cs
@@ -14,22 +14,40 @@ namespace ToDoMVC_Tests
         [TestMethod]
         public void Index_Return_All_Tasks()
         {
+            //Create the instance of fake db context
             using var ctx = DbContextFactory.CreateUnique();
+
+            //Add to the db's TaskItem table 2 objects with hardcoded data
             ctx.TaskItems.Add(new TaskItem { Id = Guid.NewGuid(), Title = "Task A", IsComplete = false });
             ctx.TaskItems.Add(new TaskItem { Id = Guid.NewGuid(), Title = "Task B", IsComplete = true });
+
+            //Save the changes in the fake db
             ctx.SaveChanges();
 
+            //Call the controller and pass it the fake context 
             var controller = new HomeController(ctx);
 
+            //We want the result for the Index Action to be a ViewResult because the actual method is returning a View
             var result = controller.Index() as ViewResult;
+
+            //We are checking that the controller is actually returning something and not crashing
             Assert.IsNotNull(result);
 
+            /* The view is holding data in list format in the actual method 
+            Therefore, the current dummy data will be converted to a list and we 
+            will check if the returning view actually returning a list of data  */
             var model = result.Model as List<TaskItem>;
             Assert.IsNotNull(model);
 
+            //Checks if the view has exactly 2 tasks (same number of dummy tasks you inserted above)
             Assert.AreEqual(2, model.Count);
         }
 
+        /*We need to check if AddTask 
+         1. Creates a task with the user entered task title
+         2. Saves it to the DB 
+         3. Redirects to the correct view 
+        */
         [TestMethod]
         public void AddTask_AddsAllTasks()
         {
